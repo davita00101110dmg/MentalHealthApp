487d809b0df6ca4f0577a82e8da3bca8aefc2439
davita00101110exe <daviti.khvedelidze.1@btu.edu.ge>
2022-07-30 17:53:56 +0400

    Initial Commit

---
+++ MentalHealthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MentalHealthApp
+//
+//  Created by Dato Khvedelidze on 30.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
