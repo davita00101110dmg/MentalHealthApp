7bba5bda705192add1b44e05c7559a908ef22ac8
davita00101110exe <daviti.khvedelidze.1@btu.edu.ge>
2022-07-31 19:10:20 +0400

    added green color constant

--- MentalHealthApp/Helpers/Constants.swift
+++ MentalHealthApp/Helpers/Constants.swift
@@ -10,4 +10,5 @@ import UIKit
 // Initializing constant colors for the project.
 
 let mainColor = UIColor(red: 251/255, green: 249/255, blue: 219/255, alpha: 1)
-let secondaryColor = UIColor(red: 197/255, green: 105/255, blue: 95/255, alpha: 1)
+let redColor = UIColor(red: 197/255, green: 105/255, blue: 95/255, alpha: 1)
+let greenColor = UIColor(red: 80/255, green: 103/255, blue: 27/255, alpha: 1)
