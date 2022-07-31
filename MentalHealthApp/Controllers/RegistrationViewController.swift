9994ab96ccc03c3f32f1ba526ce014ef249ef821
davita00101110exe <daviti.khvedelidze.1@btu.edu.ge>
2022-07-31 19:14:16 +0400

    added gesture recognizer, function which validates the fields and prints out the outcome to the user

--- MentalHealthApp/Controllers/RegistrationViewController.swift
+++ MentalHealthApp/Controllers/RegistrationViewController.swift
@@ -21,6 +21,8 @@ class RegistrationViewController: UIViewController {
     
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     
+    @IBOutlet weak var loginSuggestionLabel: UILabel!
+    
     @IBOutlet weak var registrationOutcomeLabel: UILabel!
     
     @IBOutlet weak var signupButton: UIButton!
@@ -32,28 +34,104 @@ class RegistrationViewController: UIViewController {
     }
     
     private func setupElements() {
-        
+    
+        self.navigationController?.isNavigationBarHidden = true
         self.view.backgroundColor = mainColor
         
         // Hiding the outcome label.
         registrationOutcomeLabel.alpha = 0
         
         // Configuring components of the current view.
-        Utilities.customLabel(titleLabel, 30, "Sign Up")
+        Utilities.customLabel(titleLabel, 30, "Create your account")
         Utilities.customTextField(nameTextField, "First Name")
         Utilities.customTextField(lastnameTextField, "Last Name")
         Utilities.customTextField(emailTextField, "Email")
         Utilities.customTextField(passwordTextField, "Password")
         Utilities.customTextField(confirmPasswordTextField, "Confirm Password")
-        Utilities.customLabel(registrationOutcomeLabel, 22, "")
+        Utilities.customLabel(loginSuggestionLabel, 20, "Already have an account? Sign in here!")
+        Utilities.customLabel(registrationOutcomeLabel, 20, "Please fill all the fields correctly!")
         Utilities.customButton(signupButton, "Sign Up")
+        Utilities.highlightedText(loginSuggestionLabel, "Sign in")
+        
+        // Setting up gesture for the label.
+        setupTapGesture()
+        
+        registrationOutcomeLabel.textColor = redColor
+        registrationOutcomeLabel.font = UIFont.appRegularBoldFontWith(size: registrationOutcomeLabel.font.pointSize)
         
     }
-
-    @IBAction func signupButtonPressed(_ sender: Any) {
-        //TODO: 1. validate if all fields are filled && they are filled correctly.
-        //TODO: 2. in case of success register user and send data to the firebase database
-        //TODO: 3. return to the login vc.
+    
+    private func setupTapGesture() {
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gestureTapped))
+        gestureRecognizer.numberOfTapsRequired = 1
+        gestureRecognizer.numberOfTouchesRequired = 1
+        
+        loginSuggestionLabel.isUserInteractionEnabled = true
+        loginSuggestionLabel.addGestureRecognizer(gestureRecognizer)
     }
     
+
+    
+    private func validateFields() -> String? {
+        
+        // Check if all the fields are filled in.
+        if nameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            lastnameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            confirmPasswordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields."
+        }
+        
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let cleanedConfirmPassowrd = confirmPasswordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Checking if the password is secure
+        if Utilities.isPasswordValid(cleanedPassword) == false {
+            return "Please make sure your password is at least 8 characters, contains a number and an uppercase letter"
+        }
+        
+        if cleanedPassword != cleanedConfirmPassowrd {
+            return "Please make sure your passwords match"
+        }
+        
+        return nil
+    }
+    
+    private func showOutcume(_ message: String, _ error: Bool) {
+        registrationOutcomeLabel.text = message
+        registrationOutcomeLabel.alpha = 1
+        
+        if error {
+            registrationOutcomeLabel.textColor = redColor
+        } else {
+            registrationOutcomeLabel.textColor = greenColor
+        }
+        
+    }
+    
+    @objc func gestureTapped() {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBAction func signupButtonPressed(_ sender: Any) {
+        
+        // Validating fields
+        let error = validateFields()
+        
+        if error != nil {
+            
+            // Showing error message
+            showOutcume(error!, true)
+        } else {
+            //TODO: register user and send data to the firebase database
+            
+            showOutcume("You've succesfully registered!", false)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.navigationController?.popToRootViewController(animated: true)
+            }
+        }
+    }
 }
