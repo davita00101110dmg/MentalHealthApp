77744467acfdfcfa2b234928aea98426ef798da0
davita00101110exe <daviti.khvedelidze.1@btu.edu.ge>
2022-08-01 04:37:16 +0400

    added logic to check if the user is in the database and if so add it

--- MentalHealthApp/Controllers/LoginViewController.swift
+++ MentalHealthApp/Controllers/LoginViewController.swift
@@ -6,21 +6,16 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var titleLabel: UILabel!
-    
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var forgotPasswordLabel: UILabel!
-    
     @IBOutlet weak var errorLabel: UILabel!
-    
     @IBOutlet weak var signInButton: UIButton!
     
     
@@ -31,11 +26,10 @@ class LoginViewController: UIViewController {
     }
     
     private func setupElements() {
-    
         self.navigationController?.isNavigationBarHidden = true
         self.view.backgroundColor = mainColor
         
-        // Hide the error label.
+        // Hiding the error label.
         errorLabel.alpha = 0
         
         // Adding icon to the home page.
@@ -50,11 +44,8 @@ class LoginViewController: UIViewController {
         Utilities.customButton(signInButton, "Login")
         Utilities.highlightedText(forgotPasswordLabel, "Sign up")
         
-        // Setting up gesture for the label.
+        // Setting up gesture for the label which will lead to registration VC.
         setupTapGesture()
-        
-        errorLabel.textColor = redColor
-        errorLabel.font = UIFont.appRegularBoldFontWith(size: forgotPasswordLabel.font.pointSize)
         
     }
     
@@ -67,18 +58,59 @@ class LoginViewController: UIViewController {
         forgotPasswordLabel.addGestureRecognizer(gestureRecognizer)
     }
     
+    private func validateFields() -> String? {
+        
+        // Creating cleaned versions of the text fields
+        let cleanedEmail = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Check if all the fields are filled in
+        if cleanedEmail == "" || cleanedPassword == "" {
+            return "Please fill in all fields"
+        }
+    
+        return nil
+    }
+    
     @objc func gestureTapped() {
         guard let registrationVC = storyboard?.instantiateViewController(withIdentifier: "RegistrationViewController") as? RegistrationViewController else { return }
         
         self.navigationController?.pushViewController(registrationVC, animated: true)
-
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
-        //TODO: 1. validate login credentials. in case of success go to the new view and make it root vc.
-        //TODO: 2. in case of failure make the error visible to the user.
+        // Validate login credentials
+        let error = validateFields()
         
+        if error != nil {
+            // Error while signing in
+            Utilities.showOutcume(errorLabel, error!, true)
+        } else {
+            
+            // Creating cleaned versions of the text fields
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // Trying to sign in
+            Auth.auth().signIn(withEmail: email, password: password) { result, error in
+                if error != nil {
+                    
+                    // Can't sign in
+                    self.errorLabel.text = error!.localizedDescription
+                    self.errorLabel.alpha = 1
+                } else {
+                    
+                    // Signed in
+                    Utilities.showOutcume(self.errorLabel, "You've succesfully logged in!", false)
+                    
+                    guard let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeScreenViewController") as? HomeScreenViewController else { return }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        self.view.window?.rootViewController = homeVC
+                        self.view.window?.makeKeyAndVisible()
+                    }
+                }
+            }
+        }
     }
-    
 }
-
