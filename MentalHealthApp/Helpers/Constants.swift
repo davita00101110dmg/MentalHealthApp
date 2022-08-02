04ab068508651c67aec8c7a900d36bee5d6b5cba
davita00101110exe <daviti.khvedelidze.1@btu.edu.ge>
2022-08-02 18:16:30 +0400

    add gray color constant

--- MentalHealthApp/Helpers/Constants.swift
+++ MentalHealthApp/Helpers/Constants.swift
@@ -12,3 +12,4 @@ import UIKit
 let mainColor = UIColor(red: 251/255, green: 249/255, blue: 219/255, alpha: 1)
 let redColor = UIColor(red: 197/255, green: 105/255, blue: 95/255, alpha: 1)
 let greenColor = UIColor(red: 80/255, green: 103/255, blue: 27/255, alpha: 1)
+let grayColor = UIColor(red: 0, green: 0, blue: 0.0980392, alpha: 0.22) 
