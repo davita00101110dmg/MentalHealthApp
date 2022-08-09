56ceb5e8e4bd8ee5763f0500e440db0ad4050aa4
davita00101110exe <daviti.khvedelidze.1@btu.edu.ge>
2022-08-09 22:10:36 +0400

    add image constants, sizes and colors

--- MentalHealthApp/Helpers/Constants.swift
+++ MentalHealthApp/Helpers/Constants.swift
@@ -8,15 +8,27 @@
 import UIKit
 
 // Initializing constant colors for the project.
-let mainColor = UIColor(red: 251/255, green: 249/255, blue: 219/255, alpha: 1)
+let mainColor = UIColor(red: 252/255, green: 251/255, blue: 221/255, alpha: 1)
 let redColor = UIColor(red: 197/255, green: 105/255, blue: 95/255, alpha: 1)
-let greenColor = UIColor(red: 80/255, green: 103/255, blue: 27/255, alpha: 1)
+let greenColor = UIColor(red: 145/255, green: 163/255, blue: 98/255, alpha: 1)
+let lightGreenColor = UIColor(red: 238/255, green: 243/255, blue: 223/255, alpha: 1)
 let grayColor = UIColor(red: 0, green: 0, blue: 0.0980392, alpha: 0.22)
-let lightGreen = UIColor(red: 238/255, green: 243/255, blue: 223/255, alpha: 1)
+let whiteColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
 
-// Initializing tab bar item size and their images.
-let tabBarItemSize = CGSize(width: 30, height: 30)
-let activeHomeImage = UIImage(named: "home_active")?.imageResized(to: tabBarItemSize)
-let inactiveHomeImage = UIImage(named: "home_inactive")?.imageResized(to: tabBarItemSize)
-let activeProfileImage = UIImage(named: "user_active")?.imageResized(to: tabBarItemSize)
-let inactiveProfileImage = UIImage(named: "user_inactive")?.imageResized(to: tabBarItemSize)
+// Initializing icon sizes
+let defaultIconSize = CGSize(width: 30, height: 30)
+let biggerIconSize = CGSize(width: 75, height: 75)
+let biggestIconSize = CGSize(width: 275, height: 275)
+
+// Initializing images
+let activeHomeImage = UIImage(named: "home_active")!.imageResized(to: defaultIconSize)
+let inactiveHomeImage = UIImage(named: "home_inactive")!.imageResized(to: defaultIconSize)
+let activeProfileImage = UIImage(named: "user_active")!.imageResized(to: defaultIconSize)
+let inactiveProfileImage = UIImage(named: "user_inactive")!.imageResized(to: defaultIconSize)
+
+let heartEmpty = UIImage(named: "heart_empty")?.imageResized(to: defaultIconSize)
+let heartFilled = UIImage(named: "heart_filled")?.imageResized(to: defaultIconSize)
+
+let arrowTriangle = UIImage(systemName: "arrowtriangle.forward.circle.fill")?.imageResized(to: biggerIconSize).withTintColor(greenColor)
+
+let cuteBrain = UIImage(named: "CuteBrain")?.imageResized(to: biggestIconSize)
