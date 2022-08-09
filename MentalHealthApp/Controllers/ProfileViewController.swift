67b8a4fa3611e29e888a674c9fa747ac9347e9fd
davita00101110exe <daviti.khvedelidze.1@btu.edu.ge>
2022-08-09 22:15:33 +0400

    setup UI, implement logout functionality

--- MentalHealthApp/Controllers/ProfileViewController.swift
+++ MentalHealthApp/Controllers/ProfileViewController.swift
@@ -10,25 +10,53 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     @IBOutlet weak var logoutButton: UIButton!
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var profilePictureImage: UIImageView!
+    @IBOutlet weak var fullnameLabel: UILabel!
+    @IBOutlet weak var yourLikedQuotesLabel: UILabel!
+    @IBOutlet weak var collectionViewOutlet: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupCollectionView()
         setupElements()
     }
     
-    private func setupElements() {
-        self.view.backgroundColor = lightGreen
+    private func setupCollectionView() {
+        collectionViewOutlet.delegate = self
+        collectionViewOutlet.dataSource = self
+        collectionViewOutlet.registerNib(class: QuoteCell.self)
+        collectionViewOutlet.layer.cornerRadius = 20
+        collectionViewOutlet.backgroundColor = whiteColor
         
-        logoutButton.setTitle("Logout", for: .normal)
+        let customFlowLayout = UICollectionViewFlowLayout()
+        customFlowLayout.minimumLineSpacing = 10
+        customFlowLayout.scrollDirection = .vertical
+        collectionViewOutlet.collectionViewLayout = customFlowLayout
+    }
+    
+    private func setupElements() {
+        self.view.backgroundColor = lightGreenColor
+        
+        // Adding system image temprorary
+        profilePictureImage.image = UIImage(systemName: "person.crop.circle")
+        profilePictureImage.tintColor = .black
+        
+        // Configuring components of the current view.
+        Utilities.customLabel(for: titleLabel, size: 28, text: "Profile")
+        Utilities.customLabel(for: fullnameLabel, size: 20, text: "Dato Khvedelidze") //TODO: Text should be full name variable
+        Utilities.customLabel(for: yourLikedQuotesLabel, size: 28, text: "Your liked quotes👇🏻")
+        Utilities.customButton(for: logoutButton, title: "Logout", cornerRadius: 10, color: redColor)
     }
     
     @IBAction func logoutAction(_ sender: Any) {
-        // TODO: make alert do you really want to sign out?
-        let rootNavVC = storyboard?.instantiateViewController(withIdentifier: "rootNavigationController")
 
-        self.view.window?.rootViewController = rootNavVC
-        self.view.window?.makeKeyAndVisible()
+        // Presents bottomsheet with a question if user really wants to sign out
+        guard let sheetPresentationController = storyboard?.instantiateViewController(withIdentifier: "BottomSheetViewController") as? BottomSheetViewController else { return }
+
+        present(sheetPresentationController, animated: true)
+
     }
 
 }
