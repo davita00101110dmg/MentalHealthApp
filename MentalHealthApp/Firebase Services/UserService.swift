8bd962d9975dfc875cdc07e3059bb13147a2d5c3
davita00101110exe <daviti.khvedelidze.1@btu.edu.ge>
2022-09-02 02:43:50 +0400

    create user service file which registers user in firebase database, fetches the user when needed and updates user's data

---
+++ MentalHealthApp/Firebase Services/UserService.swift
@@ -0,0 +1,77 @@
+//
+//  UserService.swift
+//  MentalHealthApp
+//
+//  Created by Dato Khvedelidze on 01.09.22.
+//
+
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
+import UIKit
+
+//MARK: - User Service
+
+struct UserService {
+    
+    private static let db = Firestore.firestore()
+    private static let collectionName = "users"
+    
+    private static var userUid: String {
+        Auth.auth().currentUser?.uid ?? ""
+    }
+    private static var document: DocumentReference {
+        db.collection(collectionName).document(userUid)
+    }
+    
+    //MARK: - Registration in database
+    
+    static func registerInDB(with name: String, lastname: String, uid: String, label: UILabel) {
+        
+        document.setData([
+            "firstname": name,
+            "lastname": lastname,
+            "uid": uid,
+            "liked_quotes": []
+        ]) { error in
+            if error != nil {
+                Utilities.showOutcume(for: label, message: "Can't save user", isError: true)
+            }
+        }
+    }
+    
+    //MARK: - Fetch User
+    
+    static func fetchUser(completion: @escaping (User) -> Void) {
+        
+        document.addSnapshotListener { documentSnapshot, error in
+            guard let document = documentSnapshot else {
+                print("Error fetching document: \(error!)")
+                return
+            }
+            
+            let createdUser = User(snapshot: document)
+            
+            completion(createdUser)
+        }
+        
+    }
+    
+    //MARK: - Update liked quotes array
+    
+    static func updateLikedQuotesArray(condition: Bool, quote: String) {
+        
+        switch condition {
+        case true:
+            document.updateData([
+                "liked_quotes": FieldValue.arrayRemove([quote])
+            ])
+            
+        case false:
+            document.updateData([
+                "liked_quotes": FieldValue.arrayUnion([quote])
+            ])
+        }
+    }
+    
+}
