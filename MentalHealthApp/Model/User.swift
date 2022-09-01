961e1cd45ef46a39c1376ed31384d72ccf8401d2
davita00101110exe <daviti.khvedelidze.1@btu.edu.ge>
2022-09-02 02:44:00 +0400

    create user model

---
+++ MentalHealthApp/Model/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  MentalHealthApp
+//
+//  Created by Dato Khvedelidze on 15.08.22.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+
+// MARK: - User
+
+struct User: Decodable {
+    let firstname: String
+    let lastname: String
+    let uid: String
+    let liked_quotes: [String]
+    
+    init(snapshot: DocumentSnapshot) {
+        let snapshotValue = snapshot.data()
+        firstname = snapshotValue?["firstname"] as? String ?? "No name"
+        lastname = snapshotValue?["lastname"] as? String ?? "No lastname"
+        uid = snapshotValue?["uid"] as? String ?? "No uid"
+        liked_quotes = snapshotValue?["liked_quotes"] as? [String] ?? ["Empty"]
+    }
+}
